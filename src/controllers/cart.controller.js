d35489a98cd2f75d288c159cb971af79088728bc
Muhammad Zain <zmunawar872@gmail.com>
2026-06-11 17:13:59 +0500

    Refactor cart controller for improved readability and consistency in SQL queries

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -28,7 +28,6 @@ const addToCart = async (req, res) => {
     `;
 
     return res.json({ success: true, data: newItem[0] });
-
   } catch (error) {
     return res.status(500).json({ error: "Server error" });
   }
@@ -39,14 +38,24 @@ const getCart = async (req, res) => {
 
   try {
     const items = await sql`
-      SELECT * FROM cart WHERE user_id = ${user_id}`;
-
-      return res.json({ success: true, data: items });
-    
+    SELECT
+      cart.id,
+      cart.user_id,
+      cart.product_id,
+      cart.quantity,
+      products.name,
+      products.image,
+      products.price
+      FROM cart
+      INNER JOIN products
+      ON cart.product_id = products.id
+      WHERE cart.user_id = ${user_id}
+      `;
+    return res.json({ success: true, data: items });
   } catch (error) {
     return res.status(500).json({ error: "Server error" });
   }
-}
+};
 
 const clearCart = async (req, res) => {
   const user_id = req.user.id;
@@ -55,32 +64,31 @@ const clearCart = async (req, res) => {
     await sql`
       DELETE FROM cart WHERE user_id = ${user_id}`;
 
-      return res.json({ success: true, message: "Cart cleared successfully" });
+    return res.json({ success: true, message: "Cart cleared successfully" });
   } catch (error) {
     return res.status(500).json({ error: "Server error" });
   }
-}
+};
 
 const decreaseQuantity = async (req, res) => {
-  const { product_id, quantity} = req.body;
+  const { product_id, quantity } = req.body;
   const user_id = req.user.id;
 
   try {
     const updatedCart = await sql`
       UPDATE cart     
       SET quantity = quantity - ${quantity}
-      WHERE user_id = ${user_id} AND product_id = ${product_id} RETURNING *`
+      WHERE user_id = ${user_id} AND product_id = ${product_id} RETURNING *`;
 
-      // id quantity <= 0 then delete the item from cart
-      if (updatedCart[0]?.quantity <= 0) {
-        await sql`
+    // id quantity <= 0 then delete the item from cart
+    if (updatedCart[0]?.quantity <= 0) {
+      await sql`
           DELETE FROM cart WHERE user_id = ${user_id} AND product_id = ${product_id}`;
-      }
-      return res.json({ success: true, data: updatedCart[0] });
-
+    }
+    return res.json({ success: true, data: updatedCart[0] });
   } catch (error) {
     return res.status(500).json({ error: "Server error" });
   }
-}
+};
 
-export {addToCart, getCart, clearCart, decreaseQuantity}
+export { addToCart, getCart, clearCart, decreaseQuantity };
