23d3b654bb6ea9e829bb0314a9797e84d6514e8e
Muhammad Zain <zmunawar872@gmail.com>
2026-06-18 22:49:28 +0500

    Add CORS middleware to enable cross-origin requests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import http from "http";
 import { Server } from "socket.io";
-
+import cors from "cors";
 import app from "./app.js";
 import initDB from "./db/database.js";
 
 const PORT = process.env.PORT || 5000;
+
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      process.env.CLIENT_URL,
+    ],
+    credentials: true,
+  })
+);
 
 // Create HTTP server
 const server = http.createServer(app);
